9ee687633433d9106c9ec47da3891e18d0da53c4
Ayoubminaei <Ayyob.rainbow@gmail.com>
2026-02-15 12:55:02 +0100

    Update App.tsx

--- pharmastudy-simple/src/App.tsx
+++ pharmastudy-simple/src/App.tsx
@@ -2967,7 +2967,7 @@ onClick={() => {
                     </button>
                   </div>
                   {/* Tabs pour filtrer par type */}
-              <div className="flex gap-2 mb-6">
+              <div className="flex gap-2 mb-6 overflow-x-auto pb-2">
                 <button
                   onClick={() => setTopicTab('all')}
                   className={`px-4 py-2 rounded-lg font-medium transition-all ${
